e1afb1bc5f1988e9ff7ec4f4834546a00b0e0b09
Shayla Lane <lanesha@oregonstate.edu>
2021-01-26 14:39:15 -0800

    Add anonymous login.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,8 @@ import 'screens/form_hub.dart';
 import 'screens/bills.dart';
 import 'screens/user_form.dart';
 import 'firebase.dart';
+import 'screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-
-
 
 const mainColor = Color(0XFF2979FF);
 
@@ -21,96 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'The Conscientious Consumer',
-      theme: ThemeData(
-        primaryColor: mainColor,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'The Conscientious Consumer'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      drawer: new Drawer(
-        child: ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: new Text('Shayla Lane'),
-              accountEmail: new Text('test@test.com'),
-              currentAccountPicture: new CircleAvatar(
-                backgroundImage: new AssetImage('assets/redhair.jpg'),
-              ),
-            ),
-            new ListTile(
-                title: Text('Submit Data'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) => new FormHub()));
-                }),
-            new ListTile(
-                title: Text('Bills'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) => new Bills()));
-                }),
-            new ListTile(
-                title: Text('Login'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) => new UserForm()));
-                }),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Wrapper(),
     );
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,26 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/client.dart';
 
 class AuthService {
-  // final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create User object based on Firebase user
+  Client _userFromFirebaseUser(User user) {
+    return user != null ? Client(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+  Stream<Client> get user {
+    return _auth.authStateChanges()
+      .map((User user) => _userFromFirebaseUser(user));
+  }
 
   //sign in anon
   Future signInAnon() async {
     try {
-      // AuthResult result = await _auth.signInAnonymously();
-      // User user = result.user;
-      // return user;
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
