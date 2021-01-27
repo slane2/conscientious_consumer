ea561e4ccaa6bed1933c1be386d7f31ce57e7dc3
Shayla Lane <lanesha@oregonstate.edu>
2021-01-27 10:43:40 -0800

    Add form validation to sign up page.

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -12,10 +12,13 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  //keeps track of the state of the form
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -31,30 +34,46 @@ class _RegisterState extends State<Register> {
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
+            key: _formKey,
             child: Column(children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                  validator: (val) =>
+                      val.length < 6 ? 'Enter a password 6+ chars long' : null,
                   obscureText: true,
                   onChanged: (val) {
                     setState(() => password = val);
                   }),
               SizedBox(height: 20.0),
               RaisedButton(
-                  color: Colors.tealAccent[400],
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(color: Colors.teal[800]),
-                  ),
-                  onPressed: () async {
-                    print(email);
-                    print('Sign Up');
-                  })
+                color: Colors.tealAccent[400],
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.teal[800]),
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        email, password);
+                    if (result == null) {
+                      setState(() =>
+                        error = 'Please supply a valid email');
+                    }
+                  }
+                }
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 12.0),
+              )
             ]),
           )),
     );
