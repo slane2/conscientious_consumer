7469b5fe0f29f7408f61667c35e4a036d7b4177c
Shayla Lane <lanesha@oregonstate.edu>
2021-02-03 10:37:53 -0800

    Fix bill form to submit to database (wip).

--- lib/screens/bill_form.dart
+++ lib/screens/bill_form.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/bill.dart';
 import '../shared/constants.dart';
-
 
 class BillFields {
   String name;
@@ -23,105 +23,131 @@ class _BillFormState extends State<BillForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              TextFormField(
-                  autofocus: true,
-                  decoration: textInputDecoration.copyWith(hintText: 'Bill Name'),
-                  onSaved: (value) {
-                    bill.name = value;
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter the name of the bill';
-                    } else {
-                      return null;
-                    }
-                  }),
-                TextFormField(
-                    autofocus: true,
-                    decoration: textInputDecoration.copyWith(hintText: 'Description'),
-                    onSaved: (value) {
-                      bill.description = value;
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a description of the bill';
-                      } else {
-                        return null;
-                      }
-                    }),
-                TextFormField(
-                    autofocus: true,
-                    decoration: textInputDecoration.copyWith(hintText: 'Source Name'),
-                        border: OutlineInputBorder()),
-                    onSaved: (value) {
-                      bill.source_name = value;
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the name of the source';
-                      } else {
-                        return null;
-                      }
-                    }),
-                TextFormField(
-                    autofocus: true,
-                    decoration: textInputDecoration.copyWith(hintText: 'Source URL'),
-                    onSaved: (value) {
-                      bill.source_url = value;
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the URL of the source';
-                      } else {
-                        return null;
-                      }
-                    }),
-                TextFormField(
-                    autofocus: true,
-                    decoration: textInputDecoration.copyWith(hintText: 'Year(s)'),
-                    onSaved: (value) {
-                      bill.year = value;
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the year or years that the bill was considered';
-                      } else {
-                        return null;
-                      }
-                    }),
-                TextFormField(
-                    autofocus: true,
-                    decoration: textInputDecoration.copyWith(hintText: 'Did the Bill Pass?'),
-                    onSaved: (value) {
-                      bill.passed = value;
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter whether the bill was passed';
-                      } else {
-                        return null;
-                      }
-                    }),
-                SizedBox(height: 10),
-                RaisedButton(
-                    onPressed: () async {
-                      if (formKey.currentState.validate()) {
-                        formKey.currentState.save();
-                        Navigator.of(context).pop();
-                      }
-                    },
-                    child: Text('Save Entry'))
-              ],
-            ),
-          )),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('bills').snapshots(),
+        builder: (content, snapshot) {
+            return Material(
+              child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                            autofocus: true,
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Bill Name'),
+                            onSaved: (value) {
+                              bill.name = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter the name of the bill';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        TextFormField(
+                            autofocus: true,
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Description'),
+                            onSaved: (value) {
+                              bill.description = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter a description of the bill';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        TextFormField(
+                            autofocus: true,
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Source Name'),
+                            onSaved: (value) {
+                              bill.source_name = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter the name of the source';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        TextFormField(
+                            autofocus: true,
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Source URL'),
+                            onSaved: (value) {
+                              bill.source_url = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter the URL of the source';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        TextFormField(
+                            autofocus: true,
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Year(s)'),
+                            onSaved: (value) {
+                              bill.year = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter the year or years that the bill was considered';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        TextFormField(
+                            autofocus: true,
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Did the Bill Pass?'),
+                            onSaved: (value) {
+                              bill.passed = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter whether the bill was passed';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        SizedBox(height: 10),
+                        RaisedButton(
+                          onPressed: () async {
+                            if (formKey.currentState.validate()) {
+                              // formKey.currentState.save();
+                              // Navigator.of(context).pop();
+                              FirebaseFirestore.instance
+                                  .collection('bills')
+                                  .add({
+                                'name': bill.name,
+                                'description': bill.description,
+                                'source_name': bill.source_name,
+                                'source_url': bill.source_url,
+                                'year': bill.year,
+                                'passed': bill.passed
+                              });
+                            }
+                          },
+                          child: Text('Save Entry')
+                        ),
+                        ElevatedButton(
+                          child: Text('Back'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }, 
+                        ),
+                      ],
+                    ),
+                  )),
+            );
+        });
   }
 }
