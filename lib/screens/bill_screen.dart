1d97577a62621538ec8f45b9f0a8f60c8b970e1b
Shayla Lane <lanesha@oregonstate.edu>
2021-02-24 09:31:01 -0800

    Add star rating widget and show it displayed in the bills listview.

--- lib/screens/bill_screen.dart
+++ lib/screens/bill_screen.dart
@@ -1,67 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'list_views/bills.dart';
 import 'form_hub.dart';
 import 'user_form.dart';
 import '../services/auth.dart';
+import '../models/bill.dart';
 
-class BillScreen extends StatefulWidget {
-  @override
-  _BillScreenState createState() => _BillScreenState();
-}
-
-class _BillScreenState extends State<BillScreen> {
-
+class BillScreen extends StatelessWidget {
   final AuthService _auth = AuthService();
+  final Bill bill;
+
+  BillScreen({Key key, @required this.bill}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Bill Name"), actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Logout'),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ]),
-        drawer: new Drawer(
-          child: ListView(
-            children: <Widget>[
-              new UserAccountsDrawerHeader(
-                accountName: new Text('Shayla Lane'),
-                accountEmail: new Text('test@test.com'),
-                currentAccountPicture: new CircleAvatar(
-                  backgroundImage: new AssetImage('assets/redhair.jpg'),
-                ),
+      appBar: AppBar(title: Text(bill.name), actions: <Widget>[
+        FlatButton.icon(
+          icon: Icon(Icons.person),
+          label: Text('Logout'),
+          onPressed: () async {
+            await _auth.signOut();
+          },
+        )
+      ]),
+      drawer: new Drawer(
+        child: ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('Shayla Lane'),
+              accountEmail: new Text('test@test.com'),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new AssetImage('assets/redhair.jpg'),
               ),
-              new ListTile(
-                  title: Text('Submit Data'),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) => new FormHub()));
-                  }),
-              new ListTile(
-                  title: Text('Bills'),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) => new Bills()));
-                  }),
-              new ListTile(
-                  title: Text('Login'),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) => new UserForm()));
-                  }),
-            ],
-          ),
+            ),
+            new ListTile(
+                title: Text('Submit Data'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) => new FormHub()));
+                }),
+            new ListTile(
+                title: Text('Bills'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) => new Bills()));
+                }),
+            new ListTile(
+                title: Text('Login'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) => new UserForm()));
+                }),
+          ],
         ),
-        body: Center(child: Column()));
+      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('bills').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Material(child: Center());
+            }
+          }),
+    );
   }
 }
