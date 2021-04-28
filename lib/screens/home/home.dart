fe9e72578e23596df48bb3e62e7c690548836729
Shayla Lane <lanesha@oregonstate.edu>
2021-04-28 09:18:52 -0700

    Clean up homepage overflow and deprecated button.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -45,7 +45,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar:
           AppBar(title: Text("The Conscientious Consumer"), actions: <Widget>[
-        FlatButton.icon(
+        TextButton.icon(
           icon: Icon(Icons.person),
           label: Text('Logout'),
           onPressed: () async {
@@ -57,200 +57,202 @@ class _HomeState extends State<Home> {
         child: ListView(
           children: <Widget>[
             new UserAccountsDrawerHeader(
-              accountName: new Text('Shayla Lane'),
-              accountEmail: new Text('test@test.com'),
-              currentAccountPicture: new CircleAvatar(
-                backgroundImage: new AssetImage('assets/redhair.jpg'),
-              ),
+        accountName: new Text('Shayla Lane'),
+        accountEmail: new Text('test@test.com'),
+        currentAccountPicture: new CircleAvatar(
+          backgroundImage: new AssetImage('assets/redhair.jpg'),
+        ),
             ),
             new ListTile(
-                title: Text('Submit Data'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) => new FormHub()));
-                }),
+          title: Text('Submit Data'),
+          onTap: () {
+        Navigator.push(
+            context,
+            new MaterialPageRoute(
+                builder: (BuildContext context) => new FormHub()));
+          }),
             new ListTile(
-                title: Text('Bills'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) => new Bills()));
-                }),
+          title: Text('Bills'),
+          onTap: () {
+        Navigator.push(
+            context,
+            new MaterialPageRoute(
+                builder: (BuildContext context) => new Bills()));
+          }),
             new ListTile(
-                title: Text('Certifications'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              new Certifications()));
-                }),
+          title: Text('Certifications'),
+          onTap: () {
+        Navigator.push(
+            context,
+            new MaterialPageRoute(
+                builder: (BuildContext context) =>
+                    new Certifications()));
+          }),
             new ListTile(
-                title: Text('Login'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (BuildContext context) => new UserForm()));
-                }),
+          title: Text('Login'),
+          onTap: () {
+        Navigator.push(
+            context,
+            new MaterialPageRoute(
+                builder: (BuildContext context) => new UserForm()));
+          }),
           ],
         ),
       ),
       body: Center(
-        child: Column(children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.blueAccent[700],
-                      )),
-                      padding: EdgeInsets.fromLTRB(5.0, 8.0, 374.3, 15.0),
-                      child: Icon(
-                        Icons.search,
-                        color: Colors.blueAccent[700],
-                        size: 30.0,
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        margin: EdgeInsets.all(3),
-                        child: FlatButton(
-                          onPressed: () {},
-                          color: Colors.transparent,
-                          child: Text(
-                            'Top  Companies',
-                            style: TextStyle(height: 5, fontSize: 10),
-                          ),
+        child: SingleChildScrollView(
+                  child: Column(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.blueAccent[700],
                         )),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: companies.map((company) {
-                    return Container(
-                      color: Colors.white,
-                      padding: EdgeInsets.all(5.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            company.name,
-                            style: TextStyle(height: 2, fontSize: 10),
-                          ),
-                          Container(
-                              child: Image.network(company.logo),
-                              height: 80.0,
-                              width: 80.0),
-                          Container(
-                              child: Image.network(
-                                  'https://upload.wikimedia.org/wikipedia/commons/f/f2/Five_star_insignia.png',
-                                  height: 5.0,
-                                  width: 100.0)),
-                        ],
+                        padding: EdgeInsets.fromLTRB(5.0, 8.0, 320, 15.0),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.blueAccent[700],
+                          size: 30.0,
+                        ),
                       ),
-                    );
-                  }).toList(),
-                ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.all(3),
+                          child: FlatButton(
+                            onPressed: () {},
+                            color: Colors.transparent,
+                            child: Text(
+                              'Top  Companies',
+                              style: TextStyle(height: 5, fontSize: 10),
+                            ),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: companies.map((company) {
+                      return Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.all(5.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              company.name,
+                              style: TextStyle(height: 2, fontSize: 10),
+                            ),
+                            Container(
+                                child: Image.network(company.logo),
+                                height: 80.0,
+                                width: 80.0),
+                            Container(
+                                child: Image.network(
+                                    'https://upload.wikimedia.org/wikipedia/commons/f/f2/Five_star_insignia.png',
+                                    height: 5.0,
+                                    width: 100.0)),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        margin: EdgeInsets.all(3),
-                        child: FlatButton(
-                          onPressed: () {},
-                          color: Colors.transparent,
-                          child: Text(
-                            'Top  Certifications',
-                            style: TextStyle(height: 5, fontSize: 10),
-                          ),
-                        )),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: companies.map((company) {
-                    return Container(
-                      color: Colors.white,
-                      padding: EdgeInsets.all(5.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            company.name,
-                            style: TextStyle(height: 2, fontSize: 10),
-                          ),
-                          Container(
-                              child: Image.network(company.logo),
-                              height: 80.0,
-                              width: 80.0),
-                          Container(
-                              child: Image.network(
-                                  'https://upload.wikimedia.org/wikipedia/commons/f/f2/Five_star_insignia.png',
-                                  height: 5.0,
-                                  width: 100.0)),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.all(3),
+                          child: FlatButton(
+                            onPressed: () {},
+                            color: Colors.transparent,
+                            child: Text(
+                              'Top  Certifications',
+                              style: TextStyle(height: 5, fontSize: 10),
+                            ),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: companies.map((company) {
+                      return Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.all(5.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              company.name,
+                              style: TextStyle(height: 2, fontSize: 10),
+                            ),
+                            Container(
+                                child: Image.network(company.logo),
+                                height: 80.0,
+                                width: 80.0),
+                            Container(
+                                child: Image.network(
+                                    'https://upload.wikimedia.org/wikipedia/commons/f/f2/Five_star_insignia.png',
+                                    height: 5.0,
+                                    width: 100.0)),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        margin: EdgeInsets.all(3),
-                        child: FlatButton(
-                          onPressed: () {},
-                          color: Colors.transparent,
-                          child: Text(
-                            'Top  Products',
-                            style: TextStyle(height: 5, fontSize: 10),
-                          ),
-                        )),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: companies.map((company) {
-                    return Container(
-                      color: Colors.white,
-                      padding: EdgeInsets.all(5.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            company.name,
-                            style: TextStyle(height: 2, fontSize: 10),
-                          ),
-                          Container(
-                              child: Image.network(company.logo),
-                              height: 80.0,
-                              width: 80.0),
-                          Container(
-                              child: Image.network(
-                                  'https://upload.wikimedia.org/wikipedia/commons/f/f2/Five_star_insignia.png',
-                                  height: 5.0,
-                                  width: 100.0)),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.all(3),
+                          child: FlatButton(
+                            onPressed: () {},
+                            color: Colors.transparent,
+                            child: Text(
+                              'Top  Products',
+                              style: TextStyle(height: 5, fontSize: 10),
+                            ),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: companies.map((company) {
+                      return Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.all(5.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              company.name,
+                              style: TextStyle(height: 2, fontSize: 10),
+                            ),
+                            Container(
+                                child: Image.network(company.logo),
+                                height: 80.0,
+                                width: 80.0),
+                            Container(
+                                child: Image.network(
+                                    'https://upload.wikimedia.org/wikipedia/commons/f/f2/Five_star_insignia.png',
+                                    height: 5.0,
+                                    width: 100.0)),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
 
 
-          ],
+            ],
+          ),
         ),
       )
       // floatingActionButton: FloatingActionButton(
